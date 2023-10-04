c5bbee062760003a70f3fb3a2d481f759a030afc
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-10-04 10:29:58 +0000

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for i in range(len(str)):
+        if ord(str[i]) in range(97, 123):
+            result += chr(ord(str[i]) - 32)
+        else:
+            result += str[i]
+
+        print("{}.format(result)")
