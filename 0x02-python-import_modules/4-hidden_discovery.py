87fb4b7936fd6cae6cf00eac29dcb4f0532c6bb3
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-10-06 02:13:01 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ import hidden_4
 if __name__ == "__main__":
     names = dir(hidden_4)
 
-    for i in range(len(hidden_4)):
+    for i in range(len(names)):
         if names[i][:2] != '__':
             print(names[i])
