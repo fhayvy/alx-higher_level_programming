4a03b2e54f2cc3ef0cf7be7bc396da53b542e662
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-10-06 02:08:52 +0000

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+
+    for i in range(len(hidden_4)):
+        if def_names[i][:2] != '__':
+            print(def_names[i])
