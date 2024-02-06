d0046c0d3c4fd31c02101228ebf0b9213a5899ae
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-06 01:20:29 +0000

    task 0 fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,8 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end="")
             printed_elements += 1
 
-    except (IndexError):
-        break
+        except (IndexError):
+            pass
 
-    finally:
-        print()
-        return printed_elements
+    print()
+    return printed_elements
