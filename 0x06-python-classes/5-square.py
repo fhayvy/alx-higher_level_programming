ee2550e0acb1a702ce6c815e37f5b912fda1b205
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-09 18:32:45 +0000

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
+"""Defines a square"""
+
+
 class Square:
+    """Blueprint for creating Square objects"""
 
-    def __init__(self,size=0):
+    def __init__(self, size=0):
+        """Initailizes the square
+        Args:
+            size: The size of the square
+        """
+
         self.__size = size
-
 
     @property
     def size(self):
-        return (self.__size)
+        """Retrieves the size of the square"""
 
+        return (self.__size)
 
     @size.setter
     def size(self, value):
+        """Sets the value of the size
+        Args:
+            value: The size of the square
+        Raises:
+            ValueError: If size is less than 0
+            TypeError: If size is not an integer
+        """
+
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("size must be >= 0")
@@ -20,12 +37,14 @@ class Square:
 
         self.__size = value
 
-
     def area(self):
+        """Returns The area of the square"""
+
         return (self.__size ** 2)
 
-
     def my_print(self):
+        """Prints in stdout the square with the character #"""
+
         if self.__size == 0:
             print()
 
