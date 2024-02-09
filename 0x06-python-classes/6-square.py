5c683a760b321689f89e24002679d940d50cf218
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-09 19:00:42 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
-class Square:
+"""Defines a square"""
 
-    def __init__(self,size=0, position=(0, 0)):
+
+class Square:
+    """Blueprint to create a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the square object
+        Args:
+            size: The value of the size
+            position: A tuple of 2 positive integers
+        """
+
         self.__size = size
         self.__position = position
 
-
     @property
     def size(self):
-        return (self.__size)
+        """Retrieves the size of the square"""
 
+        return (self.__size)
 
     @property
     def position(self):
-        return (self.__position)
+        """Retrieves the position"""
 
+        return (self.__position)
 
     @size.setter
     def size(self, value):
+        """Sets the value of the size
+        Args:
+            value: The size of the square
+        Raises:
+            ValueError: If size is less than 0
+            TypeError: If size is not an int
+        """
+
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("size must be >= 0")
@@ -26,21 +45,30 @@ class Square:
 
         self.__size = value
 
-
     @position.setter
     def position(self, value):
+        """Sets the position
+        Args:
+            value: The position of the tuple
+        Raises:
+            TypeError: If value is not a tuple or any int in tuple < 0
+        """
+
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
+
         self.__position = position
 
-
     def area(self):
-        return (self.__size ** 2)
+        """Calculates the area of the square
+        Returns the Area
+        """
 
+        return (self.__size ** 2)
 
     def my_print(self):
         if self.__size == 0:
