0fe67f6ecab489ee99b8e4f11074548b86e56f01
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-09 10:57:36 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""Defines a square"""
+
+
 class Square:
+    """The blueprint to create a sqaure"""
+
     def __init__(self,size=0):
+        """Initializes the square
+        Args: 
+            size - Represnts the size of the square defined
+        Raises:
+            ValueError: If size is less than 0
+            TypeError: If size is not an integer
+        """
+
         if isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
+
         self.__size = size
