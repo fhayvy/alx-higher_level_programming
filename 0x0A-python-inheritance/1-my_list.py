3ef61431fc6ffee81332685c16a75b2752c6c32e
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-20 12:23:21 +0000

    task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module writes a class that inherits from list"""
+
+
+class MyList(list):
+    """A class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """Sorts the list and prints out the sorted list"""
+
+        sorted_list = sorted(list)
+        print(sorted_list)
