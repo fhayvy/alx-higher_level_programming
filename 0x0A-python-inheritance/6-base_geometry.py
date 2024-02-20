575992bf6aac36b2591554dedafd60a93ed6525c
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-20 14:31:11 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Creates a class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Creates a class BaseGeometry"""
+
+    def area(self):
+        """The method is not yet implemented"""
+
+        raise Exception("area() is not implemented")
