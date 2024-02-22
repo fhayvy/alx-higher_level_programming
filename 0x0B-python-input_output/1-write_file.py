754bde2678086c530462ebd36772bf4906d2fd75
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-22 16:08:05 +0000

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Returns the number of characters written"""
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
