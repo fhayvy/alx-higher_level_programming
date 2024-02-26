64eb65edf8dcb19799cfb3fdfeb4e9cde62cbcdf
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-26 15:40:38 +0000

    task 3, 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,6 +25,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Sets the width value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
 
         self.__width = value
 
@@ -37,6 +41,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Sets the height value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
 
         self.__height = value
 
@@ -49,6 +57,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Sets the x value"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be >= 0")
 
         self.__x = value
 
@@ -61,5 +73,14 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Sets the y value"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be >= 0")
 
         self.__y = value
+
+    def area(self):
+        """Returns the area value of the rectangle"""
+
+        return self.__height * self.__width
