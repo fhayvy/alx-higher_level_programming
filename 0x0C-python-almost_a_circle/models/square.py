dde27cd9c8df9f5bef1e23c1be9ac56bc2138da5
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-27 11:34:22 +0000

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes the square class"""
 
+        self.__size = size
+        self.__x = x
+        self.__y = y
+        self.id = None
+
         super().__init__(size, size, x, y, id)
-        self.__width = size
-        self.__height = size
+
+    def __str__(self):
+        return (f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.__size}")
