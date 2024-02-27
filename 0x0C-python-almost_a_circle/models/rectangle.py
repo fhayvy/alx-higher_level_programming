3a211f8a1c05d73baba67c6cf028a87b6b7fa9f5
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-27 14:30:21 +0000

    task 11

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the rectangle class"""
 
-        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
 
     @property
     def width(self):
@@ -25,7 +25,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Sets the width value"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -41,7 +41,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Sets the height value"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -59,7 +59,7 @@ class Rectangle(Base):
         """Sets the x value"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
 
         self.__x = value
@@ -75,7 +75,7 @@ class Rectangle(Base):
         """Sets the y value"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
 
         self.__y = value
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,12 +10,32 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes the square class"""
 
-        self.__size = size
-        self.__x = x
-        self.__y = y
+        self.size = size
+        self.x = x
+        self.y = y
         self.id = None
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Gets the size value"""
+
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size value"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+        self.__height = value
+
     def __str__(self):
-        return (f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.__size}")
+        """Defines a string representation of the class"""
+
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
