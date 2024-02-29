6d630d358bd992dee62da54d4a0ab5a6450e9fce
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-02-29 04:03:17 +0000

    task 2 test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains a function which prints out the
+first name and last name of a person
+"""
+
+import sys
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints out the first and last name of
+    a person
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
