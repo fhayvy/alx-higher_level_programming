601e8157914ae0a67fd3e5e329a9624b771209a1
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-03-04 13:10:51 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -71,5 +71,17 @@ class Square:
         return (self.__size ** 2)
 
     def my_print(self):
+        """Prints in stdout the square with the character #"""
+
         if self.__size == 0:
+            print()
+
+        for i in range(self.position[1]):
+            print("")
+
+        for i in range(self.__size):
+            for i in range(self.__position[0]):
+                print(" ", end='')
+            for i in range(self.__size):
+                print("#", end='')
             print()
