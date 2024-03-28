ff5040031ee0807180e7c138a5db67614fc1ca17
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-03-28 16:28:28 +0000

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- The script won't fail if the database hbtn_0d_2 or the user user_0d_2 already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
