87a568aa8d2ba57ea1aee9a48307446d19a3ace2
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-03-28 16:25:38 +0000

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,6 @@
 -- user_0d_2 has only SELECT privilege
 -- The script won't fail if the database hbtn_0d_2 or the user user_0d_2 already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
