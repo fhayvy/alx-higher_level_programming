96e3ad0c0042139ade655b3d3deafa34ea336051
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-03-29 11:14:34 +0000

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 -- The script won't fail if the database `hbtn_0d_usa` or table `states` already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
 );
