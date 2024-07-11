d9c322c30977fd8b47a54dba768b2b3f66b5e15b
Favour <ezefavourchiamaka01@gmail.com>
2024-07-11 16:11:04 +0000

    task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sL 0.0.0.0:5000/catch_me -o /dev/null -w "You got me!"
+#!/bin/bash
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
